313d3e8a896f5c0b08b9df77d45e4713b3802950
Sagar Mude <sagarmude7@gmail.com>
2021-03-12 12:38:22 +0530

    profile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,115 +1,155 @@
-import Navbar from '../Navbar/Navbar'
-import Footer from "../Footer/footer.js"
-import { TextField,Divider, Card,CardMedia, Container,Avatar , Button, Typography, Paper } from '@material-ui/core';
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/footer.js";
+import {
+  TextField,
+  Divider,
+  Card,
+  CardMedia,
+  Container,
+  Avatar,
+  Button,
+  Typography,
+  Paper,
+} from "@material-ui/core";
 import useStyles from "./styles.js";
-import './profile.css'
-import React from 'react'
-import img from './profilepic.png';
-import EditIcon from '@material-ui/icons/Edit';
-import PageviewIcon from '@material-ui/icons/Pageview';
-import LockIcon from '@material-ui/icons/Lock';
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import {Link as RouterLink,useHistory} from "react-router-dom";
+import "./profile.css";
+import React from "react";
+import img from "./profilepic.png";
+import EditIcon from "@material-ui/icons/Edit";
+import PageviewIcon from "@material-ui/icons/Pageview";
+import LockIcon from "@material-ui/icons/Lock";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import { Link as RouterLink, useHistory } from "react-router-dom";
 
 const Profile = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  console.log(user.profile.profilePic);
 
-    const user = JSON.parse(localStorage.getItem('profile'));
-    console.log(user.profile.profilePic);
+  return (
+    <>
+      <div className={classes.container}>
+        <Navbar />
 
-    return (
-        <>
-        <div className={classes.container}>
-            <Navbar/>
+        <Typography className={classes.heading} variant="h4">
+          My Profile
+        </Typography>
 
-            <Typography className={classes.heading} variant="h4">My Profile</Typography>
+        <Container className={classes.head}>
+          <img
+            className={classes.pic}
+            src={img}
+            alt="M"
+            width="175"
+            height="190"
+          ></img>
+          <Typography
+            variant="body1"
+            color="textPrimary"
+            className={classes.headUser}
+            variant="h6"
+          >
+            {user.profile.name}
+          </Typography>
 
-            <Container className={classes.head}>
-            
-                <img className={classes.pic} src={img} alt="M" width="175" height="190"></img>
-                <Typography variant="body1" color="textPrimary" className={classes.headUser} variant="h6">{user.profile.name}</Typography>
-            
-                <Container className={classes.headRight}>
+          <Container className={classes.headRight}>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              endIcon={<LockIcon />}
+            >
+              Change Password
+            </Button>
 
-                    <Button
-                    variant="contained"
-                    color="secondary"
-                    className={classes.button}
-                    endIcon={<LockIcon/>}>Change Password</Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              endIcon={<LibraryBooksIcon />}
+            >
+              My Ads
+            </Button>
+          </Container>
+        </Container>
 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        endIcon={<LibraryBooksIcon/>}>My Ads</Button>
-                </Container>  
+        <Container>
+          <Button component={RouterLink} to="/" >
+            Home
+          </Button>
+          <Button component={RouterLink} to="/all" >
+            Books
+          </Button>
+          <Button component={RouterLink} to="/aboutus" >
+            About Us
+          </Button>
+        </Container>
 
-            </Container>
+        <Container className={classes.body}>
+          <Typography className={classes.bodyHead}>
+            Manage Your Profile
+          </Typography>
 
-            <Container>
-            <Button component={RouterLink} to="/"  className={menuButton} >
-                            Home
-                        </Button>
-                        <Button component={RouterLink} to="/all" className={menuButton}>
-                            Books
-                        </Button>
-                        <Button component={RouterLink} to="/aboutus" className={menuButton}>
-                            About Us
-                        </Button>
-            </Container>
+          <Container className={classes.bodyFields}>
+            <Typography className={classes.bodyText} variant="h6">
+              Your Name :
+            </Typography>
+            <Typography className={classes.bodyTextValue} variant="h6">
+              {user.profile.name}
+            </Typography>
+          </Container>
+          <Divider></Divider>
 
-            
-            
-            
-            <Container className={classes.body}>
+          <Container className={classes.bodyFields}>
+            <Typography className={classes.bodyText} variant="h6">
+              Email Address :
+            </Typography>
+            <Typography className={classes.bodyTextValue} variant="h6">
+              {user.profile.email}
+            </Typography>
+          </Container>
+          <Divider></Divider>
 
-                <Typography className={classes.bodyHead}>Manage Your Profile</Typography>
+          <Container className={classes.bodyFields}>
+            <Typography className={classes.bodyText} variant="h6">
+              College Name :
+            </Typography>
+            <Typography className={classes.bodyTextValue} variant="h6">
+              {user.profile.college}
+            </Typography>
+          </Container>
+          <Divider></Divider>
 
-                <Container className={classes.bodyFields}>
-                    <Typography className={classes.bodyText} variant="h6">Your Name :</Typography>
-                    <Typography className={classes.bodyTextValue} variant="h6">{user.profile.name}</Typography>
-                </Container>
-                    <Divider></Divider>
+          <Container className={classes.bodyFields}>
+            <Typography className={classes.bodyText} variant="h6">
+              Location :
+            </Typography>
+            <Typography className={classes.bodyTextValue} variant="h6">
+              {user.profile.location}
+            </Typography>
+          </Container>
+          <Divider></Divider>
 
-                <Container className={classes.bodyFields}>
-                    <Typography className={classes.bodyText} variant="h6">Email Address :</Typography>
-                    <Typography className={classes.bodyTextValue} variant="h6">{user.profile.email}</Typography>
-                </Container>
-                    <Divider></Divider>
+          <Container className={classes.bodyFields}>
+            <Typography className={classes.bodyText} variant="h6">
+              Number of Ads Sold :
+            </Typography>
+            <Typography className={classes.bodyTextValue} variant="h6">
+              {user.profile.SoldAds}
+            </Typography>
+          </Container>
 
-                <Container className={classes.bodyFields}>
-                    <Typography className={classes.bodyText} variant="h6">College Name :</Typography>
-                    <Typography className={classes.bodyTextValue} variant="h6">{user.profile.college}</Typography>
-                </Container>
-                    <Divider></Divider>
+          <Typography className={classes.bodyText}></Typography>
+        </Container>
 
-                <Container className={classes.bodyFields}>
-                    <Typography className={classes.bodyText} variant="h6">Location :</Typography>
-                    <Typography className={classes.bodyTextValue} variant="h6">{user.profile.location}</Typography>
-                </Container>
-                    <Divider></Divider>
-
-                <Container className={classes.bodyFields}>
-                    <Typography className={classes.bodyText} variant="h6">Number of Ads Sold :</Typography>
-                    <Typography className={classes.bodyTextValue} variant="h6">{user.profile.SoldAds}</Typography>
-                </Container>
-
-                <Typography className={classes.bodyText}></Typography>
-                    
-
-            </Container>
-
-            <Avatar className={`${classes.pink } ${classes.Edit} ${classes.large}`}>
-                <EditIcon/>
-            </Avatar>
-        </div>
-        <Footer/>
-        </>
-    )
-}
+        <Avatar className={`${classes.pink} ${classes.Edit} ${classes.large}`}>
+          <EditIcon />
+        </Avatar>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Profile;
-
-
-
