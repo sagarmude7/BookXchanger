7d1d62640cd5f71f9d5f3cc2e6cdb44d84558c38
Sagar Mude <sagarmude7@gmail.com>
2021-03-23 20:16:15 +0530

    Footer updated

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import useStyles from "./style.js";
-import { Typography, CssBaseline, Container, Link } from "@material-ui/core"
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import EmailIcon from '@material-ui/icons/Email';
+import { Typography, CssBaseline, Container, Link } from "@material-ui/core";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
 
-import {Link as RouterLink,useHistory} from "react-router-dom";
+import { Link as RouterLink, useHistory } from "react-router-dom";
 
 const Footer = () => {
   const classes = useStyles();
@@ -15,94 +15,177 @@ const Footer = () => {
     <>
       <CssBaseline />
       <div className={classes.footer}>
-        <div className={classes.flexContainer} style={{flexWrap:"wrap",}}>
-          <div style={{flexGrow:"1" , flex:"auto", flexDirection:"row"}}>
+        <div className={classes.flexContainer} style={{ flexWrap: "wrap" }}>
+          <div
+            className={classes.flexbox}
+            style={{ flexGrow: "1", flex: "auto", flexDirection: "row" }}
+          >
             <Typography align="center">
-              <Link href="#" color="inherit" >
-                <img className={classes.image1} src="https://picsum.photos/80/80" alt="123" />
+              <Link href="#" color="inherit">
+                <img
+                  className={classes.image1}
+                  src="https://picsum.photos/80/80"
+                  alt="123"
+                />
               </Link>
               <br />
-              <Typography style={{ fontSize: "20px", fontWeight: "bold" }}align="center"  >
+              <Typography
+                style={{ fontSize: "20px", fontWeight: "bold" }}
+                align="center"
+              >
                 Best Recycle is Book Recycle.
               </Typography>
             </Typography>
           </div>
 
-          <div style={{flexGrow:"1" }}>
-            
-              <Typography style={{ fontSize: "25px", fontWeight: "bold", padding: "0 0 30px 0" }} align="center"  >
-                Get in Touch
+          <div className={classes.flexbox} style={{ flexGrow: "1" }}>
+            <Typography
+              style={{
+                fontSize: "25px",
+                fontWeight: "bold",
+                padding: "0 0 30px 0",
+              }}
+              align="center"
+            >
+              Get in Touch
               <br />
-              </Typography>
-              <Typography align="center">
-              <Link href="https://www.facebook.com/" target="_blank" color="inherit" >
-              <FacebookIcon className={classes.Icon} />
+            </Typography>
+            <Typography align="center">
+              <Link
+                href="https://www.facebook.com/"
+                target="_blank"
+                color="inherit"
+              >
+                <FacebookIcon className={classes.Icon} />
               </Link>
-              <Link href="https://www.instagram.com/" target="_blank" color="inherit">
-              <InstagramIcon className={classes.Icon} />
+              <Link
+                href="https://www.instagram.com/"
+                target="_blank"
+                color="inherit"
+              >
+                <InstagramIcon className={classes.Icon} />
               </Link>
-              <Link href="https://github.com/sagarmude7/Bookxchanger" target="_blank" color="inherit">
-              <GitHubIcon className={classes.Icon} />
+              <Link
+                href="https://github.com/sagarmude7/Bookxchanger"
+                target="_blank"
+                color="inherit"
+              >
+                <GitHubIcon className={classes.Icon} />
               </Link>
               <Link href="http://linkedin.com/" target="_blank" color="inherit">
-              <LinkedInIcon className={classes.Icon} />
+                <LinkedInIcon className={classes.Icon} />
               </Link>
-              <Link href="mailto:bookxchanger@gmail.com" target="_blank" color="inherit">
-              <EmailIcon className={classes.Icon} />
+              <Link
+                href="mailto:bookxchanger@gmail.com"
+                target="_blank"
+                color="inherit"
+              >
+                <EmailIcon className={classes.Icon} />
               </Link>
-              </Typography>
-           
+            </Typography>
           </div>
 
-          <div style={{flexGrow:"1" }}>
-           
-              <Typography style={{ fontSize: "20px", fontWeight: "bold" }} align="center" >
-                Quick Links
-                </Typography>
-              <Typography align="center">
-                <li style={{ listStyleType: "none", textAlign: 'center' }} >
-                  <ul style={{ listStyleType: "none", textAlign: 'center', paddingInlineStart: "0px" }}>
-                    <Link to="/" component = {RouterLink} key="Home" color="inherit">
-                      Home
-                      </Link>
-                  </ul>
-                  <ul style={{ listStyleType: "none", textAlign: 'center', paddingInlineStart: "0px" }}>
-                    <Link  to="/all" component = {RouterLink} key="Books"  color="inherit">
-                      Books
-                      </Link>
-                  </ul>
-                  <ul style={{ listStyleType: "none", textAlign: 'center', paddingInlineStart: "0px" }}>
-                    <Link  to="/aboutus" component = {RouterLink}   key="About Us"  color="inherit">
-                      About Us
-                      </Link>
-                  </ul>
-                  <ul style={{ listStyleType: "none", textAlign: 'center', paddingInlineStart: "0px" }}>
-                    <Link to="/contact" component = {RouterLink} key="Contact Us" color="inherit">
-                      Contact Us
-                      </Link>
-                  </ul>
-                </li>
-              </Typography>
+          <div className={classes.flexbox} style={{ flexGrow: "1" }}>
+            <Typography
+              style={{ fontSize: "20px", fontWeight: "bold" }}
+              align="center"
+            >
+              Quick Links
+            </Typography>
+            <Typography align="center">
+              <li style={{ listStyleType: "none", textAlign: "center" }}>
+                <ul
+                  style={{
+                    listStyleType: "none",
+                    textAlign: "center",
+                    paddingInlineStart: "0px",
+                  }}
+                >
+                  <Link
+                    to="/"
+                    component={RouterLink}
+                    key="Home"
+                    color="inherit"
+                  >
+                    Home
+                  </Link>
+                </ul>
+                <ul
+                  style={{
+                    listStyleType: "none",
+                    textAlign: "center",
+                    paddingInlineStart: "0px",
+                  }}
+                >
+                  <Link
+                    to="/all"
+                    component={RouterLink}
+                    key="Books"
+                    color="inherit"
+                  >
+                    Books
+                  </Link>
+                </ul>
+                <ul
+                  style={{
+                    listStyleType: "none",
+                    textAlign: "center",
+                    paddingInlineStart: "0px",
+                  }}
+                >
+                  <Link
+                    to="/aboutus"
+                    component={RouterLink}
+                    key="About Us"
+                    color="inherit"
+                  >
+                    About Us
+                  </Link>
+                </ul>
+                <ul
+                  style={{
+                    listStyleType: "none",
+                    textAlign: "center",
+                    paddingInlineStart: "0px",
+                  }}
+                >
+                  <Link
+                    to="/contact"
+                    component={RouterLink}
+                    key="Contact Us"
+                    color="inherit"
+                  >
+                    Contact Us
+                  </Link>
+                </ul>
+              </li>
+            </Typography>
           </div>
         </div>
 
-       
-
         <div>
-          <Typography align="center"  style={{ fontSize: "13px", position: "Centre", padding: "5px 0 5px 0", background: "black",color:"white"}} >
-            {'Copyright © '}
-            <Link color="inherit" to="/" component = {RouterLink} key="Home">
+          <Typography
+            align="center"
+            style={{
+              fontSize: "13px",
+              position: "Centre",
+              padding: "5px 0 5px 0",
+              background: "black",
+              color: "white",
+            }}
+          >
+            {"Copyright © "}
+            <Link color="inherit" to="/" component={RouterLink} key="Home">
               Bookxchanger
-            </Link>
-            {' '}{new Date().getFullYear()}{'. '}
-            
-            {'All Rights Reserved'}
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {". "}
+            {"All Rights Reserved"}
           </Typography>
         </div>
       </div>
-
     </>
-  )
+  );
 };
 
 export default Footer;
--- client/src/components/Footer/style.js
+++ client/src/components/Footer/style.js
@@ -1,11 +1,10 @@
-import { colors, makeStyles } from "@material-ui/core"
+import { colors, makeStyles } from "@material-ui/core";
 import { text } from "body-parser";
 const styles = makeStyles((theme) => ({
-
- footer: {
+  footer: {
     position: "center",
-    margin:"30px 0 0 0",
-    background: "#89f7fe"
+    margin: "30px 0 0 0",
+    background: "#400CCC",
   },
 
   flexContainer: {
@@ -14,21 +13,43 @@ const styles = makeStyles((theme) => ({
     background: "#400CCC",
     padding: "30px 0px 10px 0px",
     justifyContent: "center",
-    alignItems: "ceter"
+    alignItems: "ceter",
+  },
 
+  flexbox: {
+    width: "33%",
+    "@media (max-width : 700px)": {
+      width: "100%",
+    },
   },
 
   image1: {
     width: "70px",
     margin: "5px 0px 30px 0px ",
-    position: "centre"
+    position: "centre",
+    transition: ".5s",
+    "&:hover": {
+      transform: "scale(1.3)",
+    },
   },
 
   Icon: {
     margin: "10px 10px 10px 10px",
-    fontSize: "40px"
-  }
+    fontSize: "40px",
+    transform: "scale(.9)",
+    zIndex: "-1",
 
+    transition: ".5s",
+
+    "&:hover::before": {
+      transform: "scale(1.1)",
+    },
+
+    "&:hover": {
+      color: "#00ffff",
+      transform: "scale(1.3)",
+      textShadow: "0 0 5px #00ffff",
+    },
+  },
 }));
 export default styles;
-
