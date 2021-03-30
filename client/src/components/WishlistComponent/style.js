0fe823bea5a6cceb8a2e1d53658bee485b2e3279
Shashank2370 <shashankpagrut2370@gmail.com>
2021-03-30 18:00:33 +0530

    WishList UI

--- client/src/components/WishlistComponent/style.js
+++ client/src/components/WishlistComponent/style.js
@@ -2,24 +2,56 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   mainContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  smMargin: {
-    margin: theme.spacing(1),
+    backgroundColor: "#286FB4",
+    backgroundImage: `url(${'https://www.transparenttextures.com/patterns/concrete-wall-2.png'})`,
   },
   container:{
     paddingTop:'20px'
   },
-  actionDiv: {
-    textAlign: 'center',
-  },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(0.5),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+    color: "#df4c73",
   },
   sortButton:{
-    margin: '20px',
-  }
+    marginRight: '50px',
+    marginTop:'10px',
+    backgroundColor:"#DF4C73",
+    float:'right'  
+  },
+  formControl:{
+    backgroundColor:'white',
+    border :"1 px solid grey",
+    borderRadius  :"8px",
+    width:'300px'
+  },
+  parent: {
+    width: "100%",
+  },
+  main: {
+    border: "5px solid #000WWW",
+    backgroundSize: "cover",
+    height: "400px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    position: "relative",
+    marginLeft: "0px",
+    marginRight: "0px",
+    overflow: "none",
+    width: "100%",
+    zIndex: "-1",
+    transform: "translateY(10%,0%)",
+  },
+  heading: {
+    textAlign: "center",
+    zIndex: "10",
+    fontSize: "20px",
+    fontWeight: "400",
+    fontFamily: "'Source Code Pro', monospace",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    color: "#DF4C73",
+  },
 }));
